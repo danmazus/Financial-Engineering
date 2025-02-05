203c8ae38f341b0fe0c17d5586652ed6223afd4a
Dan Mazus <146400605+danmazus@users.noreply.github.com>
2025-02-05 12:07:27 -0500

    fixed matrix size and recomputation

--- mini_project/source.py
+++ mini_project/source.py
@@ -1,31 +1,33 @@
 import numpy as np
 import pandas as pd
 
-stocks = pd.read_csv('stock_prices.csv')
+stocks = pd.read_csv('/Users/dannymazus/Documents/GitHub/Financial-Engineering/mini_project/stock_prices.csv')
+stocks = stocks.T
 print(stocks)
 
 # Cleaning up the data to transform into a matrix for computation purposes
 price_matrix = stocks.to_numpy()
-price_matrix = np.delete(price_matrix, 0, 1).astype(float)
+price_matrix = np.delete(price_matrix, 0, 0).astype(float)
 print(price_matrix)
 
 # Initializing the excess return matrix as n x p
-excess_return_matrix = np.zeros((26,9))
+excess_return_matrix = np.zeros((9,26))
 
-# Setting first row = 0 as there are no weekly returns for the first row
-excess_return_matrix[0,:] = 0
+# Setting first column = 0 as there are no weekly returns for the first row
+excess_return_matrix[:,0] = 0
 
 # Setting amount of rows and columns for, for loop
 rows, cols = price_matrix.shape
 
 # Computing the weekly excess returns and storing it in the excess return matrix
-for i in range(1, rows):
-    for j in range(cols):
-        excess_return_matrix[i,j] = (price_matrix[i,j] / price_matrix[i-1,j]) - 1
+for i in range(rows):
+    for j in range(1, cols):
+        excess_return_matrix[i,j] = (price_matrix[i,j] / price_matrix[i,j-1]) - 1
+
+excess_return_matrix = np.delete(excess_return_matrix, 0, 1)
 
 # Converting the excess returns matrix to a pandas dataframe and setting title
-excess_return_df = pd.DataFrame(excess_return_matrix,
-                                columns = ['AAPL','MSFT','AMZN','NVDA','GOOGL','TSLA','META','BRK.B','UNH'])
+excess_return_df = pd.DataFrame(excess_return_matrix)
 
 # Printing the Excess Returns Dataframe
 title = "Weekly Excess Returns for Given Stocks"
@@ -34,23 +36,20 @@ print(f"\n{title}\n")
 print(excess_return_df)
 
 # Initializing the de-meaned matrix
-Y = np.zeros((26,9))
+Y = np.zeros((9,25))
+rows_Y, cols_Y = Y.shape
 
 # Computing the de-meaned matrix of weekly excess returns
-for j in range(cols):
-    mean = np.mean(excess_return_matrix[1:,j])
-    for i in range(1, rows):
+for i in range(rows_Y):
+    mean = np.mean(excess_return_matrix[i,:])
+    for j in range(cols_Y):
         Y[i,j] = excess_return_matrix[i,j] - mean
-
-# Deleting the first row since they do not have weekly returns
-Y = np.delete(Y, 0, 0)
 
 # Compute sample Covariance Matrix
 V = np.dot(Y, Y.T)/25
 
 # Printing the De-Meaned Matrix in a Dataframe
-de_mean_df = pd.DataFrame(Y,
-                          columns = ['AAPL','MSFT','AMZN','NVDA','GOOGL','TSLA','META','BRK.B','UNH'])
+de_mean_df = pd.DataFrame(Y)
 title = "De-Meaned Matrix of Excess Returns"
 print(f"\n{title}\n")
 print(de_mean_df)
@@ -63,7 +62,7 @@ print(V)
 V_inv = np.linalg.inv(V)
 
 # Creating Vector e
-e = np.ones(25)
+e = np.ones(9)
 
 # Compute numerator of h_C
 numer = np.dot(V_inv, e)
@@ -79,6 +78,5 @@ h_C_df = pd.DataFrame(h_C, columns = ['Holdings Percentage of each Stock'])
 title = "Holdings Vector of Portfolio C with Given Stocks"
 print(f"\n{title}\n")
 print(h_C_df)
-
 
 
